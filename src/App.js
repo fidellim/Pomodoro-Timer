d9f3d42a2f7147982021ff7ccbda243bf4bafbd0
fidellim <lim.fidel@gmail.com>
2022-10-13 23:23:00 +0400

    add pomodoro layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg'
+import Pomodoro from './layout/Pomodoro'
 
 function App() {
     return (
-        <div className="flex justify-center items-center bg-slate-800 min-w-screen min-h-screen">
-            <header className="flex flex-col justify-center items-center text-white">
-                <img
-                    src={logo}
-                    className="w-[150px] h-[150px] animate-[spin_5s_linear_infinite]"
-                    alt="logo"
-                />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div>
+            <main>
+                <Pomodoro />
+            </main>
         </div>
     )
 }
