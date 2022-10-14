1b1b43eb5d240e37e14c310564aeec91a191e10d
fidellim <lim.fidel@gmail.com>
2022-10-14 18:07:11 +0400

    style time

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -15,29 +15,34 @@ const Time = ({
     return (
         <>
             <div id={titleId}>{title}</div>
-            <input
-                id={inputId}
-                type="number"
-                value={value}
-                onChange={handleValue}
-                disabled={isPlaying}
-            />
-            <button
-                id={btnIncId}
-                onClick={handleInc}
-                className="h-6 w-6 disabled:cursor-not-allowed"
-                disabled={isPlaying}
-            >
-                <ChevronUpIcon />
-            </button>
-            <button
-                id={btnDecId}
-                onClick={handleDec}
-                className="h-6 w-6 disabled:cursor-not-allowed"
-                disabled={isPlaying}
-            >
-                <ChevronDownIcon />
-            </button>
+            <div className="relative bg-fontColor text-background rounded-md">
+                <input
+                    id={inputId}
+                    type="number"
+                    value={value}
+                    onChange={handleValue}
+                    disabled={isPlaying}
+                    className="w-full bg-transparent px-2 py-1.5 disabled:cursor-not-allowed"
+                />
+                <div className="absolute top-[50%] right-[5%] -translate-y-[50%] flex flex-col justify-center items-center z-100">
+                    <button
+                        id={btnIncId}
+                        onClick={handleInc}
+                        className="h-4 w-4 disabled:cursor-not-allowed"
+                        disabled={isPlaying}
+                    >
+                        <ChevronUpIcon />
+                    </button>
+                    <button
+                        id={btnDecId}
+                        onClick={handleDec}
+                        className="h-4 w-4 disabled:cursor-not-allowed"
+                        disabled={isPlaying}
+                    >
+                        <ChevronDownIcon />
+                    </button>
+                </div>
+            </div>
         </>
     )
 }
