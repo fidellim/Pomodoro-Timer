3ba527c44229378e394c95285580d1755106ca8b
fidellim <lim.fidel@gmail.com>
2022-10-14 17:16:34 +0400

    add height and width

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Pomodoro from './layout/Pomodoro'
 
 function App() {
     return (
-        <div>
+        <div className="flex items-center justify-center w-screen h-screen">
             <main>
                 <Pomodoro />
             </main>
